51f3208f942b4b248dfa07775e00f17d880238f7
Neeraj Kumar Shukla <132157056+Neerajshukla12@users.noreply.github.com>
2025-11-27 22:44:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+let currentAudio = null;
+
+// Play sound
+document.querySelectorAll('.btn').forEach(button => {
+  button.addEventListener('click', () => {
+    const soundName = button.getAttribute('data-sound');
+
+    if (currentAudio) {
+      currentAudio.pause();
+      currentAudio.currentTime = 0;
+    }
+
+    currentAudio = new Audio(`sounds/${soundName}.mp3`);
+    currentAudio.play();
+  });
+});
+
+// Stop sound
+document.querySelector('.stop').addEventListener('click', () => {
+  if (currentAudio) {
+    currentAudio.pause();
+    currentAudio.currentTime = 0;
+  }
+});
